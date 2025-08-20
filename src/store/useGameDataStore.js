77463e89adf578320c1e5885960e98742a297d5f
Riandy R.N <5761331+riandyrn@users.noreply.github.com>
2025-08-20 16:49:41 +0700

    feat: also add validation for retainers, spouses, & clan members;

--- src/store/useGameDataStore.js
+++ src/store/useGameDataStore.js
@@ -68,13 +68,25 @@ const useGameDataStore = create((set, get) => ({
   parseES3Data: (fileContent, originalFilename) => {
     const gameData = JSON.parse(fileContent); // Let JSON.parse throw on error
 
-    // validate money & yuanbao
+    // validate money & yuanbao fields
     if (!gameData.CGNum || !gameData.CGNum.value || !Array.isArray(gameData.CGNum.value)) {
       throw new Error("money & yuanbao not found");
     }
     // validate props
     if (!gameData.Prop_have || !gameData.Prop_have.value || !Array.isArray(gameData.Prop_have.value)) {
-      throw new Error("props not found")
+      throw new Error("props not found");
+    }
+    // validate retainers field
+    if (!gameData.MenKe_Now || !gameData.MenKe_Now.value || !Array.isArray(gameData.MenKe_Now.value)) {
+      throw new Error("retainers field not found");
+    }
+    // validate spouses field
+    if (!gameData.Member_qu || !gameData.Member_qu.value || !Array.isArray(gameData.Member_qu.value)) {
+      throw new Error("spouses field not found");
+    }
+    // validate clan members field
+    if (!gameData.Member_now || !gameData.Member_now.value || !Array.isArray(gameData.Member_now.value)) {
+      throw new Error("clan members field not found");
     }
 
     // find index storage for food, vegetables, meat
